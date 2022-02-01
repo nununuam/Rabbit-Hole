539488d527f6527a66082e6b64fc3726f5d89ec6
nununuam <nunusnuam@gmail.com>
2022-02-01 15:58:13 -0800

    Hello World is working

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 // all required code that is not our own
 require("dotenv").config();
 const express = require('express');
+const req = require("express/lib/request");
 
 
 /* ====== Internal Modules  ====== */
@@ -26,7 +27,10 @@ const PORT = 4000; // full caps signify a config variable
 /* ====== App Configuration  ====== */
 // app.set
 app.set("view engine", "ejs");
-
+app.get('/', (req, res) => {
+	console.log('here')
+	res.render("index");
+});
 /* ====== Routes  ====== */
 
 	
