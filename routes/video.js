987c619f2012103c83ae982bdad6165ed1923f34
nununuam <nunusnuam@gmail.com>
2022-02-08 12:04:09 -0800

    added the editing routes

--- routes/video.js
+++ routes/video.js
@@ -3,4 +3,5 @@ ctrl = require("../controllers/videos");
 
 router.post("/upload", ctrl.createdVideo);
 router.get("/browse", ctrl.browsing)
+router.post("/edit", ctrl.editVideo);
 module.exports = router;
